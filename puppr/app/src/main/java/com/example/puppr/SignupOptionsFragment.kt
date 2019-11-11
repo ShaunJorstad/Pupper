dbe3be7fdc1da87b0b47d06867e483320e71c2d3
David Dray <draydr17@gcc.edu>
2019-11-11 17:25:49 -0500

    fixed databinding error

--- puppr/app/src/main/java/com/example/puppr/SignupOptionsFragment.kt
+++ puppr/app/src/main/java/com/example/puppr/SignupOptionsFragment.kt
@@ -6,38 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.example.puppr.databinding.FragmentSignupOptionsBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class SignupOptionsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-                // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_options, container, false)
-    }
-
-
-//    private lateinit var binding: FragmentSignupOptionsBinding
-//
 //    override fun onCreateView(
 //        inflater: LayoutInflater, container: ViewGroup?,
 //        savedInstanceState: Bundle?
 //    ): View? {
-//        val binding = DataBindingUtil.inflate<FragmentSignupOptionsBinding>(
-//            inflater,
-//            R.layout.fragment_login, container, false
-//        )
-//
-//        binding.googleSignInButton.setOnClickListener { view: View ->
-//            view.findNavController()
-//                .navigate(R.id.action_signupOptionsFragment_to_userLoginFragment)
-//        }
-//
-//        return binding.root
+//                // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_signup_options, container, false)
+//    }
+
+
+    private lateinit var binding: FragmentSignupOptionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentSignupOptionsBinding>(
+            inflater,
+            R.layout.fragment_signup_options, container, false
+        )
+
+        binding.emailSignInButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_signupOptionsFragment_to_userLoginFragment)
+        }
+
+        return binding.root
+    }
 
 }
