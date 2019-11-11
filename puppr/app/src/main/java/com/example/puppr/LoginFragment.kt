f470375a03c4cfa6d84841b39f39ba4acfadd7d8
David Dray <draydr17@gcc.edu>
2019-11-10 21:23:53 -0500

    some login UI

---
+++ puppr/app/src/main/java/com/example/puppr/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.puppr
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.puppr.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+
+        binding.userButton.setOnClickListener{view : View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_signupOptionsFragment)}
+
+        return binding.root
+    }
+
+
+}
---
+++ puppr/app/src/main/java/com/example/puppr/SignupOptionsFragment.kt
@@ -0,0 +1,43 @@
+package com.example.puppr
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignupOptionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+                // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup_options, container, false)
+    }
+
+
+//    private lateinit var binding: FragmentSignupOptionsBinding
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        val binding = DataBindingUtil.inflate<FragmentSignupOptionsBinding>(
+//            inflater,
+//            R.layout.fragment_login, container, false
+//        )
+//
+//        binding.googleSignInButton.setOnClickListener { view: View ->
+//            view.findNavController()
+//                .navigate(R.id.action_signupOptionsFragment_to_userLoginFragment)
+//        }
+//
+//        return binding.root
+
+}
