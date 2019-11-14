df395183639da44a221e2c6aece44022ab92398e
TSqueak <WhenWeedlesFly@gmail.com>
2019-11-13 20:52:42 -0500

    Added Client View
    Added Gabe and Spencer's Client View plus navigation

---
+++ puppr/app/src/main/java/com/example/puppr/clientPreferences.kt
@@ -0,0 +1,42 @@
+package com.example.puppr
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.puppr.databinding.FragmentClientPreferencesBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class clientPreferences : Fragment() {
+
+    private lateinit var binding: FragmentClientPreferencesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate<FragmentClientPreferencesBinding>(inflater, R.layout.fragment_client_preferences,
+            container, false)
+
+        val navBottom: BottomNavigationView = binding.preferenceBottomNav
+        navBottom.selectedItemId = navBottom.menu[0].itemId
+        navBottom.menu[1].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_clientPreferences_to_clientViewDog)
+            return@setOnMenuItemClickListener true
+        }
+        navBottom.menu[2].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_clientPreferences_to_clientSavedDogs)
+            return@setOnMenuItemClickListener true
+        }
+
+        return binding.root
+    }
+}
---
+++ puppr/app/src/main/java/com/example/puppr/clientViewDog.kt
@@ -0,0 +1,64 @@
+package com.example.puppr
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.puppr.databinding.FragmentClientViewDogBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class clientViewDog : Fragment() {
+
+    private lateinit var binding: FragmentClientViewDogBinding
+    private lateinit var dogModel: ViewDogModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate<FragmentClientViewDogBinding>(inflater, R.layout.fragment_client_view_dog,
+            container, false)
+        dogModel = ViewDogModel()
+
+        val bottomNav: BottomNavigationView = binding.viewDogsBottomNav
+        bottomNav.selectedItemId = bottomNav.menu[1].itemId
+        bottomNav.menu[2].setOnMenuItemClickListener {
+
+            this.findNavController().navigate(R.id.action_clientViewDog_to_clientSavedDogs)
+            return@setOnMenuItemClickListener true
+        }
+
+        bottomNav.menu[0].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_clientViewDog_to_clientPreferences)
+            return@setOnMenuItemClickListener true
+        }
+
+        binding.likeButton.setOnClickListener {
+            Log.i("YERT", "Liked")
+        }
+
+        binding.dislikeButton.setOnClickListener {
+            generateNewCard()
+        }
+
+        return binding.root
+    }
+
+    fun generateNewCard() {
+
+        Log.i("YERT", "Generating New Card")
+        dogModel.getNewDog()
+        binding.dogName.text = dogModel.dogName
+        binding.shelterName.text = dogModel.shelterName
+        binding.dogImage.setImageResource(dogModel.dogImage)
+    }
+}
