b781fba89c8bb62fcdf90d8f7a75e3685e91b124
sarahcalvis <sarahcalvis@gmail.com>
2019-11-18 21:24:51 -0500

    Set up navigation for shelter view. Still to do: edit nav bar to customize for shelter.

--- puppr/app/src/main/java/com/example/puppr/ShelterDogs.kt
+++ puppr/app/src/main/java/com/example/puppr/ShelterDogs.kt
@@ -6,18 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.puppr.databinding.FragmentShelterDogsBinding
+import com.example.puppr.databinding.FragmentShelterInformationBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * A simple [Fragment] subclass.
  */
 class ShelterDogs : Fragment() {
-
+    private lateinit var binding: FragmentShelterDogsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shelter_dogs, container, false)
+        binding = DataBindingUtil.inflate<FragmentShelterDogsBinding>(inflater, R.layout.fragment_shelter_dogs,
+            container, false)
+        val navBottom: BottomNavigationView = binding.preferenceBottomNav
+        navBottom.selectedItemId = navBottom.menu[0].itemId
+        navBottom.menu[1].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_shelterDogs_to_uploadDog)
+            return@setOnMenuItemClickListener true
+        }
+        navBottom.menu[2].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_shelterDogs_to_shelterInformation)
+            return@setOnMenuItemClickListener true
+        }
+        return binding.root
     }
 
 
--- puppr/app/src/main/java/com/example/puppr/UploadDog.kt
+++ puppr/app/src/main/java/com/example/puppr/UploadDog.kt
@@ -6,18 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.puppr.databinding.FragmentUploadDogBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * A simple [Fragment] subclass.
  */
 class UploadDog : Fragment() {
-
+    private lateinit var binding: FragmentUploadDogBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_dog, container, false)
+        binding = DataBindingUtil.inflate<FragmentUploadDogBinding>(inflater, R.layout.fragment_upload_dog,
+            container, false)
+        val navBottom: BottomNavigationView = binding.preferenceBottomNav
+        navBottom.menu[0].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_uploadDog_to_shelterDogs)
+            return@setOnMenuItemClickListener true
+        }
+        navBottom.selectedItemId = navBottom.menu[1].itemId
+        navBottom.menu[2].setOnMenuItemClickListener {
+            this.findNavController().navigate(R.id.action_uploadDog_to_shelterInformation)
+            return@setOnMenuItemClickListener true
+        }
+
+        return binding.root
     }
 
 
