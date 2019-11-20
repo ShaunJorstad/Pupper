58ad5b93d5d236638a7c756cc9be160534548d30
sarahcalvis <sarahcalvis@gmail.com>
2019-11-20 13:09:25 -0500

    started camera stuff, was unsuccessful, but start is in comments

--- puppr/app/src/main/java/com/example/puppr/UploadDog.kt
+++ puppr/app/src/main/java/com/example/puppr/UploadDog.kt
@@ -1,7 +1,9 @@
 package com.example.puppr
 
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
@@ -9,9 +11,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.get
+import android.content.pm.PackageManager
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.puppr.databinding.FragmentUploadDogBinding
+import com.google.android.gms.common.wrappers.PackageManagerWrapper
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
@@ -19,6 +23,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
  */
 class UploadDog : Fragment() {
     private lateinit var binding: FragmentUploadDogBinding
+    val REQUEST_IMAGE_CAPTURE = 1
+    //val packageManager: PackageManager? = context?.getPackageManager()
+    //val packageManager = android.content.pm.PackageManager.FEATURE_CAMERA
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,13 +43,29 @@ class UploadDog : Fragment() {
             return@setOnMenuItemClickListener true
         }
 
-        binding.captureDogButton.setOnClickListener {
-
-
-        }
+//        binding.captureDogButton.setOnClickListener {
+//            dispatchTakePictureIntent()
+//
+//        }
 
         return binding.root
     }
+// TODO: this may need to be in a different activity
+//    private fun dispatchTakePictureIntent() {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//
+//            takePictureIntent.resolveActivity(context!!.getPackageManager())?.also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//            }
+//        }
+//    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            val imageBitmap = data.extras?.get("data") as Bitmap
+//            binding.dogImage.setImageBitmap(imageBitmap)
+//        }
+//    }
+
 
 
 }
